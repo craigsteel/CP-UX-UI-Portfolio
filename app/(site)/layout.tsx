d1e35a8ca4f1c55c7173bc8a24328932eed6852d
craigsteel <craig@craigsteel-design.com>
2024-04-18 00:19:04 +0100

    addded speed-insights

--- app/(site)/layout.tsx
+++ app/(site)/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from 'next'
 import FooterMain from '../components/FooterMain';
 import dynamic from 'next/dynamic';
 import { Open_Sans } from 'next/font/google'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 // If loading a variable font, you don't need to specify the font weight
 const open_sans = Open_Sans({
@@ -27,7 +28,8 @@ export default function RootLayout({ children }: {
     <html lang="en" className={`${open_sans.variable} font-sans`} >
       <body>
         <DynamicNavbarMobile />
-          {children}
+        {children}
+        <SpeedInsights />
         <FooterMain />
       </body>
     </html>
