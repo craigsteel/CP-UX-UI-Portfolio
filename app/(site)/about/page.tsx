f4748df85cabf10a34ce25d31ea05cd975641d33
craigsteel <craig@craigsteel-design.com>
2025-07-28 23:29:08 +0100

    added brand message to about page

--- app/(site)/about/page.tsx
+++ app/(site)/about/page.tsx
@@ -20,9 +20,7 @@ export default function About() {
       <Template>
         <Hero
           heading='About Me'
-          message="A deep-seated interest in design has propelled my career across advertising, branding, and UX / UI design. My journey commenced in the dynamic environment of advertising, where I cultivated a robust work ethic and honed my ability to deliver under tight deadlines while fostering collaborative relationships with high-caliber creative teams. This experience ignited a passion for brand development, motivating me to transition to a design consultancy.
-          There, I excelled in crafting compelling brand narratives across diverse media platforms, encompassing annual reports, corporate brochures, and marketing campaigns for esteemed clients such as American Express, Manchester United, British Airways, and Mitsubishi.
-          For the past decade, I have seamlessly transitioned into the field of UX / UI design and development. I specialise in the creation of intuitive and user-centric experiences for e-commerce, corporate, and SaaS web applications."
+          message="With over two decades of dedicated experience, I am a seasoned UX/UI designer committed to crafting intuitive and impactful digital experiences that drive tangible business growth. I specialise in translating complex challenges into elegant, user-centred solutions that not only delight users but also deliver measurable commercial success for my clients."
           subheading=''>
         </Hero>
         <main className="mx-5 md:mx-10 md:mt-10">
